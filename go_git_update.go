c98c2b4be9cd7580d54d9b225a10f52ab8d1503c
longw <vbz007@hotmail.com>
2019-03-29 18:08:08 +0800

    使用errgroup进行重新实现，看起来更优雅

--- go_git_update.go
+++ go_git_update.go
@@ -4,8 +4,10 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"io"
 	"io/ioutil"
 	"os"
@@ -15,17 +17,29 @@ import (
 )
 
 const (
-	Major_Ver               = "1.2"
+	Major_Ver               = "2.1"
 	DEFAULT_MAX_CHILD_TASKS = 20
 	s_PathSeparator         = string(os.PathSeparator)
 )
 
 var (
 	wg                *sync.WaitGroup
-	b_mt              *bool
-	s_home_rootpath   *string
-	i_max_child_tasks *uint
+	b_mt              bool
+	s_home_rootpath   string
+	i_max_child_tasks uint
 )
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s, Version: %s\n", os.Args[0], Major_Ver)
+		fmt.Println("git batch update(pull)! by K.o.s[vbz276@gmail.com]!")
+		flag.PrintDefaults()
+	}
+	flag.BoolVar(&b_mt, "mt", false, "enable Multithreading")
+	flag.UintVar(&i_max_child_tasks, "ctasks", DEFAULT_MAX_CHILD_TASKS, "max Child tasks 1~20")
+	//home_rootpath := `F:\GoPortWin\go\src`
+	flag.StringVar(&s_home_rootpath, "dir", filepath.Dir(os.Args[0]), "Set Home RootPath")
+}
 
 //判断文件或文件夹是否存在
 func Exist(filename string) bool {
@@ -34,14 +48,16 @@ func Exist(filename string) bool {
 }
 
 func execCommand(commandName string, params []string, Dir_env string) bool {
-	if *b_mt {
-		defer wg.Done()
+	if b_mt {
+		if wg != nil {
+			defer wg.Done()
+		}
 	}
 	cmd := exec.Command(commandName, params...)
 	cmd.Dir = Dir_env
 	//显示运行的命令
 	//fmt.Println(cmd.Args)
-	if !*b_mt {
+	if !b_mt {
 		fmt.Println("Dir:", Dir_env)
 	}
 	stdout, err := cmd.StdoutPipe()
@@ -60,7 +76,7 @@ func execCommand(commandName string, params []string, Dir_env string) bool {
 		}
 		out_buff.WriteString(line)
 	}
-	if *b_mt {
+	if b_mt {
 		fmt.Println("Dir:", Dir_env)
 	}
 	fmt.Println(out_buff.String())
@@ -75,7 +91,7 @@ func git_Update_byDir(s_rootPath string, ch chan struct{}) {
 	}
 	git_pull := func(spath string) {
 		execCommand("git", []string{"pull"}, spath)
-		if *b_mt {
+		if b_mt {
 			<-ch
 		}
 	}
@@ -83,7 +99,7 @@ func git_Update_byDir(s_rootPath string, ch chan struct{}) {
 		if vFile.IsDir() {
 			s_gitFolder := s_rootPath + s_PathSeparator + vFile.Name() + s_PathSeparator + ".git"
 			if Exist(s_gitFolder) {
-				if *b_mt {
+				if b_mt {
 					wg.Add(1)
 					//在此处阻塞比在git_pull中阻塞要好一些
 					ch <- struct{}{}
@@ -100,33 +116,89 @@ func git_Update_byDir(s_rootPath string, ch chan struct{}) {
 	}
 }
 
-func Init() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s, Version: %s\n", os.Args[0], Major_Ver)
-		fmt.Println("git batch update(pull)! by K.o.s[vbz276@gmail.com]!")
-		flag.PrintDefaults()
+type result struct {
+	path string
+	bo   bool
+}
+
+func (r *result) print() {
+	//fmt.Printf("path:%s, git update result: %v\n", r.path, r.bo)
+}
+
+func git_Update_byDir2(root string) {
+	g, ctx := errgroup.WithContext(context.TODO())
+	paths := make(chan string)
+
+	g.Go(func() error {
+		defer close(paths)
+		return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				s_gitFolder := path + s_PathSeparator + ".git"
+				if !Exist(s_gitFolder) {
+					return nil
+				}
+			} else {
+				return nil
+			}
+			select {
+			case paths <- path:
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+			return nil
+		})
+	})
+
+	c := make(chan *result)
+	numDigesters := int(i_max_child_tasks)
+	for i := 0; i < numDigesters; i++ {
+		g.Go(func() error {
+			for po := range paths {
+				select {
+				case c <- &result{
+					path: po,
+					bo:   execCommand("git", []string{"pull"}, po),
+				}:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+			}
+			return nil
+		})
 	}
-	b_mt = flag.Bool("mt", false, "enable Multithreading")
-	i_max_child_tasks = flag.Uint("ctasks", DEFAULT_MAX_CHILD_TASKS, "max Child tasks 1~20")
-	//home_rootpath := `F:\GoPortWin\go\src`
-	s_home_rootpath = flag.String("dir", filepath.Dir(os.Args[0]), "Set Home RootPath")
+	go func() {
+		g.Wait()
+		close(c)
+	}()
+
+	for r := range c {
+		r.print()
+	}
+	if err := g.Wait(); err != nil {
+		fmt.Println("g.Wait.error:", err)
+		return
+	}
 }
 
 func main() {
-	Init()
 	flag.Parse()
 	//cap: 1~30
-	if *i_max_child_tasks == 0 || *i_max_child_tasks > DEFAULT_MAX_CHILD_TASKS {
-		*i_max_child_tasks = DEFAULT_MAX_CHILD_TASKS
+	if i_max_child_tasks == 0 || i_max_child_tasks > DEFAULT_MAX_CHILD_TASKS {
+		i_max_child_tasks = DEFAULT_MAX_CHILD_TASKS
 	}
-	ch_max_exec := make(chan struct{}, *i_max_child_tasks)
+	ch_max_exec := make(chan struct{}, i_max_child_tasks)
 	//
-	if *b_mt {
+	if b_mt {
+		/* old方法
 		wg = new(sync.WaitGroup)
-		git_Update_byDir(*s_home_rootpath, ch_max_exec)
+		git_Update_byDir(s_home_rootpath, ch_max_exec)
 		wg.Wait()
+		*/
+		git_Update_byDir2(s_home_rootpath)
 	} else {
-		git_Update_byDir(*s_home_rootpath, ch_max_exec)
+		git_Update_byDir(s_home_rootpath, ch_max_exec)
 	}
-
 }
